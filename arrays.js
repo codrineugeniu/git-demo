996909af80d94490c639da49562fa38315e9744e
Codrin Baleanu <codrin.baleanu@gmail.com>
2024-10-09 19:10:35 +0300

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,31 @@
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let result = [];
+
+// 1.
+myArr.forEach((element) => result.push(element + 1));
+
+// 2.
+for (let element of myArr) {
+  result.push(element + 1);
+}
+
+// console.log(result);
+
+// 3.
+// console.log(myArr.map((element) => element + 1));
+
+let greaterThanFive = [];
+
+for (let element of myArr) {
+  if (element >= 5) {
+    greaterThanFive.push(element);
+  }
+}
+
+console.log(myArr.map((element) => element + 1));
+
+console.log(
+  "greater than five: ",
+  myArr.filter((element) => element >= 5)
+);
